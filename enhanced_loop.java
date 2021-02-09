bd56676e2b25a1233b51c9d78c2b87be089862d3
Edmund044 <edmundopiyo@gmail.com>
2021-02-09 13:23:49 +0300

    new file:   enhanced_loop.java

---
+++ enhanced_loop.java
@@ -0,0 +1,9 @@
+class myclass{
+    public static void main (String [ ] args ){
+        int  [ ] primes = {2,3,5,7,11};
+        for(int t : primes){
+            System.out.println(t);
+        }
+
+    }
+}
