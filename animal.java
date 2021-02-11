197512848ed3ecbafb480a12e58c07f0f391546b
Edmund044 <edmundopiyo@gmail.com>
2021-02-11 09:03:23 +0300

    creating and instantiating classes

---
+++ animal.java
@@ -0,0 +1,5 @@
+public class Animal {
+    void bark(){
+        System.out.println("Woof-woof");
+    }
+}
---
+++ createclass.java
@@ -0,0 +1,7 @@
+
+class myApp {
+    public static void main (String [ ] args){
+       Animal dog = new Animal();
+       dog.bark();
+    }
+}
