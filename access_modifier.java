288eec0c8578df658f947f4aa663c9a4334959eb
Edmund044 <edmundopiyo@gmail.com>
2021-02-15 09:51:59 +0300

    access modifier practice

---
+++ access_modifier.java
@@ -0,0 +1,10 @@
+public class Vehicle{
+   private int maxspeed;
+   private int wheels;
+   private String color;
+   private double fuelCapacity;
+
+   public void horn(){
+       System.out.println("Beep! Beep!");
+   }
+}
