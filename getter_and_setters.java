5f162cbcf59afe32575bfd064057b8e207ec3721
Edmund044 <edmundopiyo@gmail.com>
2021-02-16 09:27:40 +0300

    added getters and setters class

---
+++ getter_and_setters.java
@@ -0,0 +1,10 @@
+public class getter_and_setters {
+    String color = ""; 
+
+     public void setColor(String c){
+         this.color = c;
+     }
+     public String getColor(){
+         return color; 
+     }
+}
