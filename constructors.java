1af8bdb13e699f4b254386d91c67cece092244f4
Edmund044 <edmundopiyo@gmail.com>
2021-02-17 09:46:25 +0300

    adding constructors

---
+++ constructors.java
@@ -0,0 +1,14 @@
+public class Vehicle {
+    private String color = "";
+    Vehicle(){
+        color = "red";
+    }
+    
+}
+public class Vehicle {
+    private String color = "";
+    Vehicle(String c){
+        color = c;
+    }
+
+}
