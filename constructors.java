0026f66693f5b133c1e0aec14b3d36c398436b56
Edmund044 <edmundopiyo@gmail.com>
2021-02-17 09:52:21 +0300

    implemented constructor

--- constructors.java
+++ constructors.java
@@ -1,14 +1,22 @@
-public class Vehicle {
+public class Vehicle1 {
     private String color = "";
-    Vehicle(){
+    Vehicle1(){
         color = "red";
     }
     
 }
-public class Vehicle {
+public class Vehicle2 {
     private String color = "";
-    Vehicle(String c){
+    Vehicle2(String c){
         color = c;
     }
 
+}
+public class myClass{
+    public static void main(String [ ] args){
+        Vehicle1 v1 = new Vehicle1();
+        Vehicle2 v2 = new Vehicle2("pink");
+
+
+    }
 }
