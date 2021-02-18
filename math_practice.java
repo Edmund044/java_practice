35bc3e01be1a87759835acfc825eb77761a082c5
Edmund044 <edmundopiyo@gmail.com>
2021-02-18 09:14:21 +0300

    math class practice

---
+++ math_practice.java
@@ -0,0 +1,21 @@
+public class math_practice {
+    public static void main(String [ ] args)
+    {
+      int a = Math.abs(-10);
+      double b = Math.ceil(7.345);
+      double c = Math.floor(7.234);
+      int d = Math.max(10,20);
+      int e = Math.min(10,30);
+      double f = Math.pow(2,3);
+
+      System.out.println(a);
+      System.out.println(b);
+      System.out.println(c);
+      System.out.println(d);
+      System.out.println(e);
+      System.out.println(f);
+
+
+  
+    }
+}
