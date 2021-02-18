57fc3c48c21e3fe46bfb3c74a7f53c85bf892ea3
Edmund044 <edmundopiyo@gmail.com>
2021-02-18 09:01:39 +0300

    value types

---
+++ value_types.java
@@ -0,0 +1,11 @@
+public class MyClass {
+      public static void main (String [ ] args){
+          int x = 5;
+          addOneto(5);
+          System.out.println(x);
+      }
+      static int addOneto(int c){
+          return c +1;
+      }
+
+}
