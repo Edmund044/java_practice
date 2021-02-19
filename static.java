09bc44504dba1bb00222fc7438336adc453f2999
Edmund044 <edmundopiyo@gmail.com>
2021-02-19 09:23:31 +0300

    practice on static class

---
+++ static.java
@@ -0,0 +1,14 @@
+public class static {
+    public static int COUNT = 0;
+    Counter(){
+        COUNT++;
+    }
+}
+public class MyClass{
+    public static void main(String [ ] args){
+        Counter c1 = new Counter();
+        Counter c2 = new Counter();
+        System.out.println(Counter.COUNT);
+
+    }
+}
