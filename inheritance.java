ca42d585b41d4d277200406ebd254453f8530ada
Edmund044 <edmundopiyo@gmail.com>
2021-02-22 10:22:07 +0300

    inheritance practice two

--- inheritance.java
+++ inheritance.java
@@ -1,20 +1,19 @@
-public class Vehicle {
- protected String brand = "Ford";
-
- public void honk(){
-     System.out.println("Beep! Beep!");
- }
-
+class Vehicle {
+    protected String brand = "Ford";
+    public void honk(){
+        System.out.println("Beep!! Beep!!");
+    }
 }
 
-public class car extends Vehicle{
-    private String model = "Mustang"; 
-    public static void main(String [ ] aargs){
-      car car1 = new car();
-      car1.honk();
-      System.out.println("Brand name is"+ car1.brand);
-      System.out.println("Model type is"+ car1.model);
-  }
+class Car extends Vehicle {
+   private String model = "Mustang";
+   public static void main(String [ ] args){
+       Car myCar = new Car();
 
-     
+       myCar.honk();
+       System.out.println("Brand name is:"+ myCar.brand);
+       System.out.println("Model name is:"+ myCar.model);
+
+   }
+
 }
