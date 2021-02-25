9f6882837c0cd7e6f9f57a6fb40152cb7b744d88
Edmund044 <edmundopiyo@gmail.com>
2021-02-25 09:28:58 +0300

    enum

---
+++ enums.java
@@ -0,0 +1,13 @@
+enum Level {
+    LOW,
+    MEDIUM,
+    HIGH
+}
+public class Main{
+    public static void main (String [ ] args){
+      Level myVar = Level.MEDIUM;
+      System.out.println(myVar);
+
+    }
+
+}
