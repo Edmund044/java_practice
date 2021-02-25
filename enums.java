85b4d8b2b531bbc26ee399f2cc831ca128353a5e
Edmund044 <edmundopiyo@gmail.com>
2021-02-25 09:42:39 +0300

    switch case with enum

--- enums.java
+++ enums.java
@@ -1,3 +1,5 @@
+import org.graalvm.compiler.core.phases.LowTier;
+
 enum Level {
     LOW,
     MEDIUM,
@@ -7,6 +9,21 @@ public class Main{
     public static void main (String [ ] args){
       Level myVar = Level.MEDIUM;
       System.out.println(myVar);
+
+      switch(myVar){
+          case LOW:
+           System.out.println(myVar);
+          break; 
+          case MEDIUM:
+           System.out.println(myVar);
+          break;
+          case  HIGH:
+           System.out.println(myVar);
+          break;
+          default:
+            System.out.println("Invalid choice!!");  
+
+      }
 
     }
 
