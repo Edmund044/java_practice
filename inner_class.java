5b2f96da270e435ec0b6bd48b30c650af4b9d52b
Edmund044 <edmundopiyo@gmail.com>
2021-02-26 09:28:35 +0300

    inner class correction

--- inner_class.java
+++ inner_class.java
@@ -1,16 +1,13 @@
 class OuterClass {
-    int x = 5;
+    int x =5;
     class InnerClass {
-      int y = 7;
+        int y = 10;
     }
 }
 class Main{
-    public static void main(String [ ] args){
-
-      OuterClass myOuter = new OuterClass();
-      OuterClass.InnerClass myInner = myOuter.InnerClass();
-      System.out.println(myOuter.x + myInner.y);
-
+    public static void main (String [ ] args) {
+        OuterClass myOuter = new OuterClass();
+        OuterClass.InnerClass myInner = myOuter.new InnerClass();
+        System.out.println(myOuter.x + myInner.y);
     }
-
 }
