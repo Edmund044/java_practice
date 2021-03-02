86db1ce0888b5e7148b75d8ab4d96ab492d3c6ba
Edmund044 <edmundopiyo@gmail.com>
2021-03-02 08:56:46 +0300

    array list

---
+++ arrayList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+class List{
+    public static void main (String [ ] args){
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("MISTUBISHI");
+        cars.add("TOYOTA");
+        System.out.println(cars);
+
+    }
+}
+
