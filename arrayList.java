0fe7922fd9e4601ce551ac906dc082c9b80e2d3f
Edmund044 <edmundopiyo@gmail.com>
2021-03-02 09:11:56 +0300

    setting and getting at an index

--- arrayList.java
+++ arrayList.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
+
 class List{
-    public static void main (String [ ] args){
+    public static void main (String [] args){
         ArrayList<String> cars = new ArrayList<String>();
         cars.add("Volvo");
         cars.add("BMW");
         cars.add("MISTUBISHI");
         cars.add("TOYOTA");
         System.out.println(cars);
+        System.out.println(cars.get(0));
+        cars.set(0,"Lamborghini");
         System.out.println(cars.get(0));
         
 
