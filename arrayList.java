cd91fd14f7958da54daf55c73645f878d3a1c832
Edmund044 <edmundopiyo@gmail.com>
2021-03-04 09:27:23 +0300

    sorted array

--- arrayList.java
+++ arrayList.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 class List{
     public static void main (String [] args){
@@ -20,11 +21,19 @@ class List{
         System.out.println(cars);
         System.out.println(cars.size());
         System.out.println(cars);
-        int i;
+       int i;
+       
+       for(i=0;i<cars.size();i++){
+           System.out.println(cars.get(i));
+       }
+       System.out.println("Part 2");
+        Collections.sort(cars);
         for(i=0;i<cars.size();i++){
             System.out.println(cars.get(i));
         }
-        
+        /* for(int i : cars){
+            System.out.println(cars.get(i));
+        }*/
 
     }
 }
