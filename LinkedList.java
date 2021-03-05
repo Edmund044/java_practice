de781d5f9a2233e5a3396c18d5c53d55b6195527
Edmund044 <edmundopiyo@gmail.com>
2021-03-05 09:26:25 +0300

    linked list in java

---
+++ LinkedList.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+class list{
+    public static void main (String [ ] args){
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("VOLVO");
+        cars.add("BMW");
+        cars.add("MERCEDES");
+        cars.add("AUDI");
+        Sytem.out.println(cars);
+
+    }
+}
