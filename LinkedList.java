94604e877f95c5631d75dfe79320ac28fe3e87cf
Edmund044 <edmundopiyo@gmail.com>
2021-03-05 09:29:36 +0300

    linked list get()

--- LinkedList.java
+++ LinkedList.java
@@ -6,7 +6,8 @@ class list{
         cars.add("BMW");
         cars.add("MERCEDES");
         cars.add("AUDI");
-        Sytem.out.println(cars);
+        System.out.println(cars);
+        System.out.println(cars.get(0));
 
     }
 }
