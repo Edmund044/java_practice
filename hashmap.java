fbd627f4b6f1422fa20ad775dc9f34b11eb78357
Edmund044 <edmundopiyo@gmail.com>
2021-03-08 09:17:39 +0300

    hashmap

---
+++ hashmap.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+class hash{
+    public static void main (String [ ] args){
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+        capitalCities.put("England","London");
+        capitalCities.put("France","Paris");
+        capitalCities.put("Germany","Berlin");
+        capitalCities.put("Kenya","Nairobi");
+        System.out.println(capitalCities);
+    }
+}
