1bead00d596d6b4219eb08eb457695b32eccb6f7
Edmund044 <edmundopiyo@gmail.com>
2021-03-09 09:41:31 +0300

    wrapper class

---
+++ wrapper.java
@@ -0,0 +1,14 @@
+public class wrapper {
+     public static void main (String [ ] args) {
+         Integer number1 = 5;
+         Double number2 = 12.4;
+         Character letter = 'A';
+         
+         System.out.println(number1);
+         System.out.println(number2);
+         System.out.println(letter);
+
+     } 
+
+
+}
