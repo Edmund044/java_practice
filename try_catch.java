e76d5721df7f8471496dee4dba7b3ee7eae1b7d7
Edmund044 <edmundopiyo@gmail.com>
2021-03-10 10:52:56 +0300

    try catch

---
+++ try_catch.java
@@ -0,0 +1,13 @@
+class sth {
+
+    public static void  main (String [ ] args) {
+        try {
+            int  [ ] numbers = {1,2,3};
+            System.out.println(numbers[10]);
+        }
+        catch(Exception e) {
+            System.out.println("Something went wrong!!");
+        }
+
+    }
+}
