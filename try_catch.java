fbc6d0da151f52fd639eb013483b42cf82ac9c7f
Edmund044 <edmundopiyo@gmail.com>
2021-03-10 11:14:52 +0300

    try catch 2

--- try_catch.java
+++ try_catch.java
@@ -1,13 +1,16 @@
 class sth {
 
-    public static void  main (String [ ] args) {
-        try {
-            int  [ ] numbers = {1,2,3};
-            System.out.println(numbers[10]);
-        }
-        catch(Exception e) {
+    public static void  main (String [ ] ss) {
+       try {
+           int [ ] numbers = {1,2,3,4,5};
+           System.out.println(numbers[10]);
+       }
+       catch (Exception e){
             System.out.println("Something went wrong!!");
-        }
+       }
+       finally{
+           System.out.println("Executed successfully");
+       }
 
     }
 }
