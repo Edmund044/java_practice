71fcf25b0b93028bab9aeb6fa7553d5acbd9c88e
Edmund044 <edmundopiyo@gmail.com>
2021-03-11 12:02:18 +0300

    thread

---
+++ thread.java
@@ -0,0 +1,9 @@
+public class Main extends Thread {
+      public static void main (String [ ] args){
+          public void run(){
+              System.out.println("Started inside run!!");
+          }
+
+      }
+
+}
