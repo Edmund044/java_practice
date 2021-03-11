5fddaff810bb01c85143e0b20a1b8a2b235ebdb3
Edmund044 <edmundopiyo@gmail.com>
2021-03-11 12:07:26 +0300

    thread2

--- thread.java
+++ thread.java
@@ -1,5 +1,7 @@
-public class Main extends Thread {
+public class trial extends Thread {
       public static void main (String [ ] args){
+          trial thread1 = new trial();
+          thread1.start();
           public void run(){
               System.out.println("Started inside run!!");
           }
