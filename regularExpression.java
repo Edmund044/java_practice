ddddb324ba5b0d06667be7fceed3a8d365f50270
edmund <edmundopiyo@gmail.com>
2021-03-15 11:41:49 +0300

    regular expression

--- regularExpression.java
+++ regularExpression.java
@@ -4,7 +4,16 @@ import java.util.regex.Matcher;
 public class regularExpression {
     public static void main (String [ ] args){
       Pattern pattern = Pattern.compile("W3SCHOOLS");
-      Matcher matcher = pattern.matcher("W3SCHOOLS IS COOL");
+      Matcher matcher = pattern.matcher("W3SCHOOLS IS GREAT");
+      boolean matchfound = matcher.find();
+      if(matchfound){
+           System.out.println("THERE IS A MATCH");
+
+      }
+      else{
+          System.out.println("THERE IS NO MATCH");
+      }
+
 
     }
     
