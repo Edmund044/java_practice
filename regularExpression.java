b712b0285446a711d64589cb388734f1fb451439
edmund <edmundopiyo@gmail.com>
2021-03-15 11:33:31 +0300

    regualr expression

---
+++ regularExpression.java
@@ -0,0 +1,12 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class regularExpression {
+    public static void main (String [ ] args){
+      Pattern pattern = Pattern.compile("W3SCHOOLS");
+      Matcher matcher = pattern.matcher("W3SCHOOLS IS COOL");
+
+    }
+    
+
+}
