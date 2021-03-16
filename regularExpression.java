4d123fd216775985f34d6065705b685a200f45d8
edmund <edmundopiyo@gmail.com>
2021-03-16 10:25:41 +0300

    trial iterator

--- regularExpression.java
+++ regularExpression.java
@@ -3,7 +3,7 @@ import java.util.regex.Matcher;
 
 public class regularExpression {
     public static void main (String [ ] args){
-      Pattern pattern = Pattern.compile("W3SCHOOLS");
+      Pattern pattern = Pattern.compile("yyy");
       Matcher matcher = pattern.matcher("W3SCHOOLS IS GREAT");
       boolean matchfound = matcher.find();
       if(matchfound){
---
+++ trial_iterator.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class trial_iterator {
+    public static void main (String [ ] args){
+       ArrayList<String> cars = new ArrayList<String>();
+       cars.add("VOLVO");
+       cars.add("MERCEDES");
+       cars.add("BMW");
+       cars.add("AUDI");
+       
+       System.out.println(cars);
+
+    }    
+
+}
