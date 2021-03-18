71a67b693916bc19c59899e40e2fa1f8541cef15
edmund <edmundopiyo@gmail.com>
2021-03-18 09:56:56 +0300

    java read is perfect

--- java_read.java
+++ java_read.java
@@ -1,26 +1,22 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+class readFile {
+    public static void main(String [ ] args){
+        try{
+           File obj = new File("file.txt");
+           Scanner objRead = new Scanner(obj);
+           while(objRead.hasNext()){
+               System.out.println(objRead.nextLine());
 
-class readfile{
-      public static void main(String [ ] args){
-         try{
-            File obj = new File("file.txt");
-            Scanner objRead = new Scanner(obj);
-            while(objRead.hasNextLine()){
-                System.out.println(objRead.nextLine());
-
-            }
-            objRead.close();
-         }
-        
-         catch(FileNotFoundException e){
-           System.out.println("Something went wrong!!");  
-           System.out.println(e);
-         }
-
-      }
+           }
+           objRead.close();
 
 
+        }
+        catch(FileNotFoundException e){
+          System.out.println("Something went wrong!!");
+        }
 
+    }
 }
